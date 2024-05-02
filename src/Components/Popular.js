87550b73ccd17ba18881e97632819a7a5e45515e
Quang Minh <qminh30k3@gmail.com>
2024-05-02 00:40:13 -0400

    done

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useGlobalContext } from '../context/global'
-import styled from 'styled-components'
-import Sidebar from './Sidebar'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context/global';
+import styled from 'styled-components';
+import Sidebar from './Sidebar';
 
-function Popular({rendered}) {
-    const {popularAnime,isSearch, searchResults} = useGlobalContext()
+function Popular({ rendered }) {
+    const { popularAnime, isSearch, searchResults } = useGlobalContext();
 
     const conditionalRender = () => {
-        if(!isSearch && rendered === 'popular'){
+        if (!isSearch && rendered === 'popular') {
             return popularAnime?.map((anime) => {
-                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                    <img src={anime.images.jpg.large_image_url} alt="" />
-                </Link>
-            })
-        }else{
+                return (
+                    <AnimeLink to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                        <img src={anime.images.jpg.large_image_url} alt="" />
+                    </AnimeLink>
+                );
+            });
+        } else {
             return searchResults?.map((anime) => {
-                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                    <img src={anime.images.jpg.large_image_url} alt="" />
-                </Link>
-            })
+                return (
+                    <AnimeLink to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                        <img src={anime.images.jpg.large_image_url} alt="" />
+                    </AnimeLink>
+                );
+            });
         }
-    }
+    };
 
     return (
         <PopularStyled>
@@ -30,35 +34,50 @@ function Popular({rendered}) {
             </div>
             <Sidebar />
         </PopularStyled>
-    )
+    );
 }
 
 const PopularStyled = styled.div`
     display: flex;
-    .popular-anime{
+
+    .popular-anime {
         margin-top: 2rem;
         padding-top: 2rem;
         padding-bottom: 2rem;
         padding-left: 5rem;
-        padding-right: 0;
+        padding-right: 5rem ;
         width: 100%;
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-        grid-gap: 2rem;
-        background-color: #fff;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));  /* Adjusted minimum size */
+        grid-gap: 1.5rem;  /* Smaller gaps for tighter arrangement */
+        background-color: rgb(27, 18, 18);  /* Softer white background */
         border-top: 5px solid #e5e7eb;
-        a{
-            height: 500px;
-            border-radius: 7px;
-            border: 5px solid #e5e7eb;
+        
+        a {
+            height: 400px;  /* Reduced height */
+            border-radius: 10px;  /* Softer rounding */
+            border: 4px solid #FAFAD2;  /* Brighter anime-inspired border color */
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);  /* Subtle shadow */
+
+            padding: 0.5rem;  /* Added padding for better spacing */
+            
         }
-        a img{
+
+        a img {
             width: 100%;
             height: 100%;
             object-fit: cover;
-            border-radius: 5px;
+            border-radius: 15px;  /* Increased rounding */
         }
     }
 `;
 
-export default Popular
+const AnimeLink = styled(Link)` 
+    transition: transform 0.3s ease-in-out;  /* Smooth transition effect */
+
+    &:hover {
+        transform: scale(1.05);  /* Slight enlargement on hover */
+    }
+`;
+
+export default Popular;
--- src/Components/Upcoming.js
+++ src/Components/Upcoming.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useGlobalContext } from '../context/global'
-import styled from 'styled-components'
-import Sidebar from './Sidebar'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context/global';
+import styled from 'styled-components';
+import Sidebar from './Sidebar';
 
-function Upcoming({rendered}) {
-    const {upcomingAnime ,isSearch, searchResults} = useGlobalContext()
+function Upcoming({ rendered }) {
+    const { upcomingAnime, isSearch, searchResults } = useGlobalContext();
 
     const conditionalRender = () => {
-        if(!isSearch && rendered === 'upcoming'){
+        if (!isSearch && rendered === 'upcoming') {
             return upcomingAnime?.map((anime) => {
-                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                    <img src={anime.images.jpg.large_image_url} alt="" />
-                </Link>
-            })
-        }else{
+                return (
+                    <AnimeLink to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                        <img src={anime.images.jpg.large_image_url} alt="" />
+                    </AnimeLink>
+                );
+            });
+        } else {
             return searchResults?.map((anime) => {
-                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
-                    <img src={anime.images.jpg.large_image_url} alt="" />
-                </Link>
-            })
+                return (
+                    <AnimeLink to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                        <img src={anime.images.jpg.large_image_url} alt="" />
+                    </AnimeLink>
+                );
+            });
         }
-    }
+    };
 
     return (
         <PopularStyled>
@@ -30,35 +34,48 @@ function Upcoming({rendered}) {
             </div>
             <Sidebar />
         </PopularStyled>
-    )
+    );
 }
 
 const PopularStyled = styled.div`
     display: flex;
-    .upcoming-anime{
+
+    .upcoming-anime {
         margin-top: 2rem;
         padding-top: 2rem;
         padding-bottom: 2rem;
         padding-left: 5rem;
-        padding-right: 0;
+        padding-right: 5rem ;
         width: 100%;
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-        grid-gap: 2rem;
-        background-color: #fff;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));  /* Adjusted minimum size */
+        grid-gap: 1.5rem;  /* Smaller gaps for tighter arrangement */
+        background-color: rgb(27, 18, 18);  /* Softer white background */
         border-top: 5px solid #e5e7eb;
-        a{
-            height: 500px;
-            border-radius: 7px;
-            border: 5px solid #e5e7eb;
+        
+        a {
+            height: 400px;  /* Reduced height */
+            border-radius: 10px;  /* Softer rounding */
+            border: 4px solid 	rgb(0, 150, 255);  /* Brighter anime-inspired border color */
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);  /* Subtle shadow */
+            padding: 0.5rem;  /* Added padding */
         }
-        a img{
+
+        a img {
             width: 100%;
             height: 100%;
             object-fit: cover;
-            border-radius: 5px;
+            border-radius: 15px;  /* Increased rounding */
         }
     }
 `;
 
-export default Upcoming
+const AnimeLink = styled(Link)` 
+    transition: transform 0.3s ease-in-out;  /* Smooth transition effect */
+
+    &:hover {
+        transform: scale(1.05);  /* Slight enlargement on hover */
+    }
+`;
+
+export default Upcoming;
